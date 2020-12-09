e0d3ff1cf3ea870425feb14ca6d9132b5dede294
dslovi <dslovi@godaddy.com>
2020-12-09 21:15:58 +0100

    removing robots.js, adding API

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import {robots} from './robots';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchField from './SearchField'
 import './App.css'
@@ -8,7 +7,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: 'robots',
+            robots: [],
             searchfield: ''
         }
     }
@@ -17,15 +16,31 @@ class App extends Component {
         this.setState({searchfield: event.target.value});
     }
 
-    render(){
-        const filteredRobots = robots.filter( robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
-        return (
-            <div className='tc'>
-                <h1 className='f1'>Robofriends</h1>
-                <SearchField onSearchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                    return response.json();
+                }
+            )
+            .then(users => {
+                this.setState({robots: users});
+            })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+        if (this.state.robots.length === 0) {
+            return (
+                <h1 className='f1'>Loading</h1>);
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchField onSearchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     };
 }
 

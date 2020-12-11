6454134c541e58c9bbe52295f22f457dc0a4254c
dslovi <dslovi@godaddy.com>
2020-12-11 15:10:56 +0100

    Applied Redux to robots API request

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App'
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
     <React.StrictMode>
